e16047a2b64c71476465e5f0d2242994de213221
acacia8628 <masashi.k.429@gmail.com>
2022-01-19 18:40:09 +0900

    review controller correct

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 use App\Http\Requests\ReviewRequest;
 use App\Models\Review;
 use App\Models\Shop;
@@ -17,26 +16,26 @@ class ReviewController extends Controller
         $score = $request->input('score');
         $comment = $request->input('comment');
 
-        $items = Review::create([
+        Review::create([
             'user_id' => $user_id,
             'shop_id' => $shop_id,
             'score' => $score,
             'comment' => $comment,
         ]);
-        return redirect()->back();
+        return back();
     }
 
     public function show($id)
     {
         $user_id = Auth::id();
 
-        $shop = Shop::with(['reviews'])->where('id',$id)->first();
-        $user_review = Review::where('user_id',$user_id)
-            ->where('shop_id',$id)
+        $shop = Shop::with(['reviews'])->where('id', $id)->first();
+        $user_review = Review::where('user_id', $user_id)
+            ->where('shop_id', $id)
             ->first();
         $reviews = Review::with(['shop'])
-            ->where('shop_id',$id)
-            ->orderBy('created_at','desc')
+            ->where('shop_id', $id)
+            ->orderBy('created_at', 'desc')
             ->paginate(20);
 
         $items = [
@@ -49,31 +48,27 @@ class ReviewController extends Controller
 
     public function update(ReviewRequest $request, $id)
     {
-        if(Auth::check()){
-            $user_id = Auth::id();
-            $review_id = $id;
-            $score = $request->input('score');
-            $comment = $request->input('comment');
+        $user_id = Auth::id();
+        $review_id = $id;
+        $score = $request->input('score');
+        $comment = $request->input('comment');
 
-            $item = Review::where('id',$review_id)->where('user_id',$user_id)->first();
-
-            $item->score = $score;
-            $item->comment = $comment;
-            $item->save();
-            return redirect()->back();
-        }
+        Review::where('id', $review_id)
+            ->where('user_id', $user_id)
+            ->update([
+                'score' => $score,
+                'comment' => $comment
+            ]);
+        return back();
     }
 
     public function destroy($id)
     {
-        if(Auth::check()){
-            $user_id = Auth::id();
+        $user_id = Auth::id();
 
-            $item = Review::where('id',$id)
-                ->where('user_id',$user_id)
-                ->first();
-            $item->delete();
-            return redirect()->back();
-        }
+        $item = Review::where('id', $id)
+                ->where('user_id', $user_id)
+                ->delete();
+        return back();
     }
 }
