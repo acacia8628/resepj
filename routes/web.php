57d299996f20562705bac1d29bddf7a7498250e5
acacia8628 <masashi.k.429@gmail.com>
2022-01-19 17:05:55 +0900

    route correct

--- routes/web.php
+++ routes/web.php
@@ -8,47 +8,49 @@ use App\Http\Controllers\ReserveController;
 use App\Http\Controllers\ThanksController;
 use App\Http\Controllers\ReviewController;
 
-Route::middleware(['auth','can:isAdmin'])->group(function(){
+Route::middleware(['auth', 'can:isAdmin'])->group(function(){
     
 });
 
-Route::middleware(['auth','can:isStoreManager'])->group(function(){
+Route::middleware(['auth', 'can:isStoreManager'])->group(function(){
     
 });
 
-Route::get('/',[ShopController::class,'index'])->name(
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/done', [ReserveController::class, 'index'])->name(
+        'reserve.index'
+    );
+    Route::get('/mypage', [UserController::class, 'index'])->name(
+        'user.index'
+    );
+    Route::resource('users', UserController::class)->only([
+        'show'
+    ]);
+    Route::resource('likes', LikeController::class)->only([
+        'store', 'destroy'
+    ]);
+    Route::resource('reserves', ReserveController::class)->only([
+        'store', 'edit', 'update', 'destroy'
+    ]);
+    Route::resource('reviews', ReviewController::class)->only([
+        'store', 'update', 'destroy'
+    ]);
+});
+
+Route::get('/',[ShopController::class, 'index'])->name(
     'shop.index'
 );
-Route::get('/detail/{shop_id}',[ShopController::class,'show'])->name(
+Route::get('/detail/{shop_id}',[ShopController::class, 'show'])->name(
     'shop.show'
 );
-Route::get('/done',[ReserveController::class,'index'])->middleware(['auth'])->name(
-    'reserve.index'
-);
-Route::get('/mypage',[UserController::class,'index'])->middleware(['auth'])->name(
-    'user.index'
-);
-
-Route::resource('users',UserController::class)->middleware(['auth'])->only([
-    'show'
-]);
-Route::resource('likes',LikeController::class)->middleware(['auth'])->only([
-    'store','destroy'
-]);
-Route::resource('reserves',ReserveController::class)->middleware(['auth'])->only([
-    'store','edit','update','destroy'
-]);
-Route::resource('thanks',ThanksController::class)->only([
+Route::resource('thanks', ThanksController::class)->only([
     'index'
 ]);
-Route::resource('reviews',ReviewController::class)->only([
+Route::resource('reviews', ReviewController::class)->only([
     'show'
 ]);
-Route::resource('reviews',ReviewController::class)->middleware(['auth'])->only([
-    'store','update','destroy'
-]);
 
-Route::get('/dashboard', function () {
+Route::get('/dashboard',  function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
