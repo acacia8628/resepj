daa4c199b3f89772a25ad3a80487ffc58797a82c
acacia8628 <masashi.k.429@gmail.com>
2022-01-29 20:07:31 +0900

    QrCodeController show add if

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use App\Models\Reserve;
 
 class QrCodeController extends Controller
 {
     public function show($id)
     {
-        $reserve = Reserve::find($id);
-        return view('reserve.qr_code', ['reserve' => $reserve]);
+        $reserve = Reserve::where('id', $id)
+            ->where('status', 'reserved')
+            ->first();
+
+        if (isset($reserve) && $reserve->user_id == Auth::id()) {
+            return view('reserve.qr_code', ['reserve' => $reserve]);
+        } else {
+            return redirect('mypage');
+        }
     }
 }
